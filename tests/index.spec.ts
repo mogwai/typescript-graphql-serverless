51a0b81622c2298319c37abf5201f29b65c610cf
Harry Blum <himion0@gmail.com>
2018-10-06 13:39:45 +0100

    Linter, Preitter, better config

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,13 +1,12 @@
-import test from "ava";
-import { app } from "../src/index";
-import * as chaiHttp from "chai-http";
-import * as chai from "chai";
-const { request } = chai.use(chaiHttp);
+import test from 'ava';
+import { app } from '../src/index';
+import * as chai from 'chai';
+const { request } = chai.use(require('chai-http'));
 
-test("We recieve the correct message", async t => {
+test('We recieve the correct message', async t => {
   let res = await request(app)
-    .get("/")
+    .get('/')
     .send();
-  t.is(res.body.message, "HEY!");
+  t.is(res.body.message, 'HEY!');
   t.pass();
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,31 @@
-const path = require("path");
-const slsw = require("serverless-webpack");
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+const slsw = require('serverless-webpack');
 
 module.exports = {
   entry: slsw.lib.entries,
+  mode: 'development',
   resolve: {
-    extensions: [".js", ".json", ".ts", ".tsx"]
+    extensions: ['.js', '.json', '.ts', '.tsx'],
   },
+  stats: 'errors-only',
   output: {
-    libraryTarget: "commonjs",
-    path: path.join(__dirname, ".webpack"),
-    filename: "[name].js"
+    libraryTarget: 'commonjs',
+    path: path.join(__dirname, '.webpack'),
+    filename: '[name].js',
   },
-  target: "node",
+  target: 'node',
+  externals: [nodeExternals()],
   module: {
     rules: [
       {
         test: /\.ts(x?)$/,
         use: [
           {
-            loader: "ts-loader"
-          }
-        ]
-      }
-    ]
-  }
+            lGoader: 'ts-loader',
+          },
+        ],
+      },
+    ],
+  },
 };
